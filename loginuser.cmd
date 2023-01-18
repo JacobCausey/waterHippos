c9da30dd84ec8ff043e3eb001fe2175007140a96
Jacob Causey <cau16002@byui.edu>
2023-01-18 13:41:30 -0700

    regretandchocolate

--- loginuser.cmd
+++ loginuser.cmd
@@ -1,2 +1,2 @@
-curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
-localhost:3000/login
+curl -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+
